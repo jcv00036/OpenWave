e67d9985466ea569221bd1c961366a119130d553
Juan Carrasco Vico <jcv00036@red.ujaen.es>
2026-03-01 23:37:56 +0100

    Añadido el gestor de textos para la localización de la aplicación

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,18 @@ import 'package:openwave/Nucleo/Emisora.dart';
 import 'package:openwave/OpenWave.dart';
 import 'package:openwave/Reproduccion/Reproductor.dart';
 import 'package:openwave/Reproduccion/IReproductor.dart';
+import 'package:openwave/l10n/textosApp.dart';
 
 
 Future<void> main() async {
-  WidgetsFlutterBinding();
-
-  var reproductorAudio = AudioPlayer(userAgent: 'myradioapp/1.0 (Linux;Android 11) https://myradioapp.com',
-    useProxyForRequestHeaders: true, // default
-  );
+  // WidgetsFlutterBinding();
+  //
+  // var reproductorAudio = AudioPlayer(userAgent: 'myradioapp/1.0 (Linux;Android 11) https://myradioapp.com',
+  //   useProxyForRequestHeaders: true, // default
+  // );
+  await TextosApp.cargarTextos("es");
   runApp(const OpenWave());
-  Emisora emisora = Emisora('Test', 'https://dispatcher.rndfnk.com/crtve/rne5/main/mp3/high', [], []);
-  IReproductor reproductor = Reproductor(reproductorAudio);
-  reproductor.reproducirEmisora(emisora);
+  // Emisora emisora = Emisora('Test', 'https://dispatcher.rndfnk.com/crtve/rne5/main/mp3/high', [], []);
+  // IReproductor reproductor = Reproductor(reproductorAudio);
+  // reproductor.reproducirEmisora(emisora);
 }
