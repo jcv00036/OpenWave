4026fbc284326536220bbda8166b938c6f80ad0b
Juan Carrasco Vico <jcv00036@red.ujaen.es>
2026-03-25 21:23:27 +0100

    Solucionado el problema con el mini-reproductor

--- lib/openwave_app_pantalla_principal.dart
+++ lib/openwave_app_pantalla_principal.dart
@@ -53,84 +53,89 @@ class _OpenWavePantallaPrincipalState extends State<OpenWavePantallaPrincipal> w
           Consumer<Reproductor>(
               builder: (context, manager, child) {
                 if (manager.parado) {
-                  return Container();
+                  return const SizedBox.shrink();
                 }
-                return BottomAppBar(
-                    child: InkWell(
-                      child: Row(
-                          children: [
-                            Image(image: manager.emisoraSeleccionada.imagen!.image,
-                                width: 60,
-                                height: 60,
-                                fit: BoxFit.cover),
-                            SizedBox(width: 10),
-                            Expanded(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(manager.emisoraSeleccionada.nombre, style: Theme.of(context).textTheme.titleMedium),
-                                  StreamBuilder<IcyMetadata?>(
-                                    stream: manager.metadataStream,
-                                    builder: (context, snapshot) {
-                                      final metadata = snapshot.data;
-                                      final title = metadata?.info?.title ?? '';
-                                      if (title != '') {
-                                        return SizedBox(
-                                        height: 20, // Altura suficiente para el texto labelLarge
-                                        child: Marquee(
-                                          text: "${TextosApp.getTexto("reproduciendo")}: $title",
-                                          style: Theme.of(context).textTheme.labelLarge,
-                                          scrollAxis: Axis.horizontal,
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          blankSpace: 50.0, // Espacio entre el final del texto y el inicio de la repetición
-                                          velocity: 30.0,   // Velocidad del movimiento
-                                          pauseAfterRound: const Duration(seconds: 0), // Pausa al completar una vuelta
-                                          accelerationDuration: const Duration(seconds: 1),
-                                          accelerationCurve: Curves.linear,
-                                          decelerationDuration: const Duration(milliseconds: 500),
-                                          decelerationCurve: Curves.easeOut,
-                                        ),
-                                      );
-                                      }
-                                      return Container(height: 0, padding: EdgeInsets.zero,);
-                                    },
-                                  ),
-                                ],
+                return Material(
+                  elevation: 8, // Añade una pequeña sombra para separarlo del contenido
+                  color: Theme.of(context).bottomAppBarTheme.color ?? Theme.of(context).colorScheme.surfaceContainer,
+                  child: InkWell(
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 8, top: 8),
+                        child: Row(
+                            children: [
+                              Image(image: manager.emisoraSeleccionada.imagen!.image,
+                                  width: 60,
+                                  height: 60,
+                                  fit: BoxFit.cover),
+                              SizedBox(width: 10),
+                              Expanded(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(manager.emisoraSeleccionada.nombre, style: Theme.of(context).textTheme.titleMedium),
+                                    StreamBuilder<IcyMetadata?>(
+                                      stream: manager.metadataStream,
+                                      builder: (context, snapshot) {
+                                        final metadata = snapshot.data;
+                                        final title = metadata?.info?.title ?? '';
+                                        if (title != '') {
+                                          return SizedBox(
+                                          height: 20, // Altura suficiente para el texto labelLarge
+                                          child: Marquee(
+                                            text: "${TextosApp.getTexto("reproduciendo")}: $title",
+                                            style: Theme.of(context).textTheme.labelLarge,
+                                            scrollAxis: Axis.horizontal,
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            blankSpace: 50.0, // Espacio entre el final del texto y el inicio de la repetición
+                                            velocity: 30.0,   // Velocidad del movimiento
+                                            pauseAfterRound: const Duration(seconds: 0), // Pausa al completar una vuelta
+                                            accelerationDuration: const Duration(seconds: 1),
+                                            accelerationCurve: Curves.linear,
+                                            decelerationDuration: const Duration(milliseconds: 500),
+                                            decelerationCurve: Curves.easeOut,
+                                          ),
+                                        );
+                                        }
+                                        return Container(height: 0, padding: EdgeInsets.zero,);
+                                      },
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ),
 
-                            IconButton(
-                                onPressed: manager.retrocederEmisora,
-                                icon: Icon(Icons.skip_previous),
-                                style: ButtonStyle(visualDensity: VisualDensity.compact),),
-                            ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                shape: const CircleBorder(),
-                                fixedSize: const Size(60, 60),
-                                 padding: EdgeInsets.zero,
-                                 backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+                              IconButton(
+                                  onPressed: manager.retrocederEmisora,
+                                  icon: Icon(Icons.skip_previous),
+                                  style: ButtonStyle(visualDensity: VisualDensity.compact),),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  shape: const CircleBorder(),
+                                  fixedSize: const Size(60, 60),
+                                   padding: EdgeInsets.zero,
+                                   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+                                ),
+                                child: SizedBox(
+                                    width: 30,
+                                    height: 30,
+                                    child: manager.cargando
+                                           ? CircularProgressIndicator()
+                                           : Icon(Icons.stop_outlined, size: 30),
+                                ),
+                                onPressed: () => manager.pararReproduccion(),
                               ),
-                              child: SizedBox(
-                                  width: 30,
-                                  height: 30,
-                                  child: manager.cargando
-                                         ? CircularProgressIndicator()
-                                         : Icon(Icons.stop_outlined, size: 30),
-                              ),
-                              onPressed: () => manager.pararReproduccion(),
-                            ),
-                            IconButton(
-                              onPressed: manager.pasarEmisora,
-                              icon: Icon(Icons.skip_next),
-                              style: ButtonStyle(visualDensity: VisualDensity.compact),
-                            )
-                          ]
+                              IconButton(
+                                onPressed: manager.pasarEmisora,
+                                icon: Icon(Icons.skip_next),
+                                style: ButtonStyle(visualDensity: VisualDensity.compact),
+                              )
+                            ]
+                        ),
                       ),
                       onTap: () => {
                         Navigator.push(context, MaterialPageRoute(builder: (context) => PantallaReproduccion()))
                       },
-                    )
+                    ),
                 );
               }
           ),
