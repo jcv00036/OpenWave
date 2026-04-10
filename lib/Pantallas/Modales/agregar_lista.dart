0605d1d3fa6e2eb7483332cc101cd92170de3c4c
Juan Carrasco Vico <jcv00036@red.ujaen.es>
2026-04-10 17:09:21 +0200

    Arreglado el bug con la edición de listas

--- lib/Pantallas/Modales/agregar_lista.dart
+++ lib/Pantallas/Modales/agregar_lista.dart
@@ -63,7 +63,7 @@ class _PantallaAgregarListaState extends State<PantallaAgregarLista> {
                 textAlign: TextAlign.center,
               ),
               TextField(
-                enabled: !widget._modoEditar,
+                enabled: !widget._listaEditar!.permanente,
                 decoration: InputDecoration(
                   labelText: TextosApp.getTexto("nombre_lista"),
                   border: OutlineInputBorder(
